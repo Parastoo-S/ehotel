4a52c9059037510489fb5c820647aedd6e754364
Parastoo-S <psaha091@uottawa.ca>
2019-03-28 10:52:17 -0400

    Method to add chain phone numbers

--- app/controllers/chain_controller.rb
+++ app/controllers/chain_controller.rb
@@ -12,6 +12,7 @@ class ChainController < ApplicationController
     @chain = Chain.new(chain_params)
     if @chain.save
       create_emails
+      create_phone_numbers
       flash[:notice] = "Chain created successfully."
       redirect_to root_path
     else
@@ -24,10 +25,20 @@ class ChainController < ApplicationController
     if params['emails'].present?
       email_arr = params[:emails].split(',')
       email_arr.each do |email|
-        ChainEmail.create(email_address: email, chain_id: @chain.id)
+        ChainEmail.create(email_address: email.gsub(/\s+/,''), chain_id: @chain.id)
       end
     end
   end
+
+  def create_phone_numbers
+    if params['phones'].present?
+      phone_arr = params[:phones].split(',')
+      phone_arr.each do |phone|
+        ChainPhoneNumber.create(phone_number: phone.gsub(/\s+/,''), chain_id: @chain.id)
+      end
+    end
+  end
+
   private
     def chain_params
       params.require(:chain).permit(:name , address_attributes: [:street_number, :street_name, :apt_number, :city, :state, :zip])
