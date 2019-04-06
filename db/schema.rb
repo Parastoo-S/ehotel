c00bd2f074d2586e6250183f5d5cb7c57c23d79b
mahdietemad <mahdietemad12@hotmail.com>
2019-04-05 20:16:13 -0400

    nc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190402232402) do
+ActiveRecord::Schema.define(version: 20190404234155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -126,7 +126,9 @@ ActiveRecord::Schema.define(version: 20190402232402) do
     t.bigint "address_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
     t.index ["address_id"], name: "index_users_on_address_id"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "amenities", "rooms"
